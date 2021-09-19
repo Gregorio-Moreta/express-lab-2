2c5f60bfd696307d0398c798de720daf0ff1b225
Gregorio Moreta <gregoriomoreta4@gmail.com>
2021-09-18 21:46:23 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/greeting/:name', (req, res) => {
   res.send( 'Hello, it has been a long time, ' + req.params.name + ".");
 });
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+  console.log('requesting a tip percentage');
+  res.send( "Thanks for dining with us! your total will be $" + req.params.total * (req.params.tipPercentage / 100) + ".");
+});
+
 app.listen(5000, () => {
   console.log('Express is listening for requests from the browser');
 });
